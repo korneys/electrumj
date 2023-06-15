459864a26a9aa60d564822d52b1ab220cc39723d
Sergey Korney <skornei@users.noreply.github.com>
2023-06-15 16:38:38 +0300

    Update ScriptPubKey.java

--- src/main/java/org/electrumj/dto/transactionget/ScriptPubKey.java
+++ src/main/java/org/electrumj/dto/transactionget/ScriptPubKey.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Data
 @ToString
-@JsonIgnoreProperties  
+@JsonIgnoreProperties(ignoreUnknown = true)  
 public class ScriptPubKey {
     private String asm;
     private String hex;
